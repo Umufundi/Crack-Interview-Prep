0e40e107e116a391c85714b5a85e4f89ec5382fe
Flo <96528163+Umufundi@users.noreply.github.com>
2024-02-21 10:59:55 -0500

    Time: 66 ms (96.02%), Space: 16.9 MB (82.82%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,31 +1,16 @@
-class Solution(object):
-    def findItinerary(self, tickets):
-        """
-        :type tickets: List[List[str]]
-        :rtype: List[str]
-        """
-        from collections import defaultdict
-        self.flightMap = defaultdict(list)
-
-        for ticket in tickets:
-            origin, dest = ticket[0], ticket[1]
-            self.flightMap[origin].append(dest)
-
-        # sort the itinerary based on the lexical order
-        for origin, itinerary in self.flightMap.items():
-        # Note that we could have multiple identical flights, i.e. same origin and destination.
-            itinerary.sort(reverse=True)
-
-        self.result = []
-        self.DFS('JFK')
-
-        # reconstruct the route backwards
-        return self.result[::-1]
-
-    def DFS(self, origin):
-        destList = self.flightMap[origin]
-        while destList:
-            #while we visit the edge, we trim it off from graph.
-            nextDest = destList.pop()
-            self.DFS(nextDest)
-        self.result.append(origin)
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict(list)
+        
+        for src, dst in sorted(tickets, reverse=True):
+            graph[src].append(dst)
+        
+        stack = ["JFK"]
+        itinerary = []
+        
+        while stack:
+            while graph[stack[-1]]:
+                stack.append(graph[stack[-1]].pop())
+            itinerary.append(stack.pop())
+        
+        return itinerary[::-1]
