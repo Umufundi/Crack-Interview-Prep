52264cdc5191c8691f2b9202b6d33c382f2a0300
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-07 10:37:09 -0500

    Time: 126 ms (85.00%), Space: 17.1 MB (32.39%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,10 +1,20 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        # add zeros at the ends
         flowerbed=[0]+flowerbed+[0]
+        
+        #iterate over indexes
         for i in range(1,len(flowerbed)-1):
-            if flowerbed[i]==0 and flowerbed[i+1]==0 and flowerbed[i-1]==0:
+            
+            # check if 3 items in the list are consecutively zero
+            if (flowerbed[i]==0 
+                and flowerbed[i+1]==0 
+                and flowerbed[i-1]==0):
+                
+                # if so check item at i to 1 and decrement n
                 flowerbed[i]=1
                 n-=1
+        # after the iteration, if n is zero or lower return true        
         if n<=0:
             return True
         return False
