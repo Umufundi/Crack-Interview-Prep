335a8cd3b86e517aef2a4124b2d81e5dd8a1ba62
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-07 17:49:53 -0500

    Time: 121 ms (69.82%), Space: 17.4 MB (30.71%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -1,46 +1,23 @@
 class Solution:
-  def removeInvalidParentheses(self, s: str) -> List[str]:
-    def getLeftAndRightCounts(s: str) -> tuple:
-      l = 0
-      r = 0
-
-      for c in s:
-        if c == '(':
-          l += 1
-        elif c == ')':
-          if l == 0:
-            r += 1
-          else:
-            l -= 1
-
-      return l, r
-
-    def isValid(s: str):
-      count = 0  # Number of '(' - # Of ')'
-      for c in s:
-        if c == '(':
-          count += 1
-        elif c == ')':
-          count -= 1
-        if count < 0:
-          return False
-      return True  # Count == 0
-
-    ans = []
-
-    def dfs(s: str, start: int, l: int, r: int) -> None:
-      if l == 0 and r == 0 and isValid(s):
-        ans.append(s)
-        return
-
-      for i in range(start, len(s)):
-        if i > start and s[i] == s[i - 1]:
-          continue
-        if r > 0 and s[i] == ')':  # Delete s[i]
-          dfs(s[:i] + s[i + 1:], i, l, r - 1)
-        elif l > 0 and s[i] == '(':  # Delete s[i]
-          dfs(s[:i] + s[i + 1:], i, l - 1, r)
-
-    l, r = getLeftAndRightCounts(s)
-    dfs(s, 0, l, r)
-    return ans
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        def valid(s):
+            l,r=0,0
+            for c in s:
+                if c=='(':l+=1
+                elif c==')':
+                    if  l<=0:r+=1
+                    else:l-=1
+            return not l and not r
+        res=[];seen=set();level={s}
+        while True:
+            newLevel=set()
+            for word in level:
+                if valid(word):res.append(word)
+            if res: return res 
+            for word in level:
+                for i in range(len(word)):
+                    if word[i] in '()':
+                        newWord=word[:i]+word[i+1:];
+                        if newWord not in seen:seen.add(newWord);newLevel.add(newWord)       
+            level=newLevel        
+        return [""]
