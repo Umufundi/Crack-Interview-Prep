5292e2abef360aada9de3d9a6bacdd2ab53d6d27
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-08 10:46:33 -0500

    Time: 372 ms (99.33%), Space: 21 MB (34.72%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,22 +1,23 @@
+from collections import defaultdict
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        triplets = set()
-        for i in range(len(nums) - 2):
-            firstNum = nums[i]
-            j = i + 1
-            k = len(nums) - 1
-            while j < k:
-                secondNum  = nums[j]
-                thirdNum = nums[k]
+        neg = defaultdict(int);pos = defaultdict(int);zeros = 0 
+        for x in nums:
+            if x < 0:neg[x] += 1
+            elif x > 0:pos[x] += 1
+            else:zeros += 1    
+        r = []       
+        if zeros:
+            for n in neg:
+                if -n in pos:r.append((0, n, -n))
+            if zeros > 2:
+                r.append((0,0,0))
+        for set_a, set_b in ((neg, pos), (pos, neg)):
+            set_a_items = list(set_a.items())
+            for i, (x, q) in enumerate(set_a_items):
+                for x2, q2 in set_a_items[i:]:
+                    if x != x2 or (x == x2 and q > 1):
+                        if -x-x2 in set_b:
+                            r.append((x, x2, -x-x2))
 
-                potentialSum = firstNum + secondNum + thirdNum 
-                if potentialSum > 0:
-                    k -= 1
-                elif potentialSum < 0:
-                    j += 1
-                else:
-                    triplets.add((firstNum , secondNum ,thirdNum))
-                    j += 1
-                    k -= 1
-        return triplets
+        return r
