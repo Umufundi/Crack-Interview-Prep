70cc6ceae0b67f32e750bd0ef31d425f7fdcd3ff
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-09 18:41:33 -0500

    Time: 82 ms (97.76%), Space: 18 MB (55.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,13 +1,20 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        dp = [float('inf')] * n
-        dp[src] = 0
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+                
+        graph = {i:[] for i in range(n)}
+        for start, finish, cost in flights:
+            graph[start].append((finish,cost))       
+        q = deque([(src,0)])
+        visited=[-1]*n
+        while q and K > -1:
+            s=len(q)
+            while s > 0:
+                city,cost = q.popleft()
 
-        for _ in range(k + 1):
-            temp = dp[:]
-            for flight in flights:
-                if dp[flight[0]] != float('inf'):
-                    temp[flight[1]] = min(temp[flight[1]], dp[flight[0]] + flight[2])
-            dp = temp
-        
-        return dp[dst] if dp[dst] != float('inf') else -1
+                for i,j in graph[city]:
+                    if visited[i] == -1 or j+cost < visited[i]:
+                        q.append((i,j+cost))
+                        visited[i]=j+cost
+                s -= 1
+            K -= 1
+        return visited[dst]
