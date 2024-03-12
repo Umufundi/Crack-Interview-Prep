25ec8c7290e2823e3923e13a1a635909042d3e8a
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-12 17:16:06 -0400

    Time: 36 ms (93.62%), Space: 16.7 MB (32.89%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,33 +1,24 @@
-class Solution(object):
-    def uniquePathsWithObstacles(self, obstacleGrid):
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
 
-        # If the starting cell has an obstacle, then simply return as there would be
-        # no paths to the destination.
-        if obstacleGrid[0][0] == 1:
-            return 0
+        dp = [[-1 for i in range(n)] for j in range(m)]
 
-        # Number of ways of reaching the starting cell = 1.
-        obstacleGrid[0][0] = 1
+        def move(r,c):
+            if r >=m or c >=n:
+                return 0
 
-        # Filling the values for the first column
-        for i in range(1,m):
-            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+            if obstacleGrid[r][c] == 1:
+                return 0
 
-        # Filling the values for the first row        
-        for j in range(1, n):
-            obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+            if r == m-1 and c == n-1:
+                return 1
 
-        # Starting from cell(1,1) fill up the values
-        # No. of ways of reaching cell[i][j] = cell[i - 1][j] + cell[i][j - 1]
-        # i.e. From above and left.
-        for i in range(1,m):
-            for j in range(1,n):
-                if obstacleGrid[i][j] == 0:
-                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
-                else:
-                    obstacleGrid[i][j] = 0
+            if dp[r][c] != -1:
+                return dp[r][c]
+                
+            dp[r][c] = move(r+1,c) + move(r,c+1)
+            return dp[r][c]
 
-        # Return value stored in rightmost bottommost cell. That is the destination.            
-        return obstacleGrid[m-1][n-1]
+        return move(0,0)
