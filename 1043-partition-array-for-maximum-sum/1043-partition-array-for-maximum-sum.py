30de12bc962e47375b9b4c1f1cddfc159ddfdd14
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-12 14:40:24 -0400

    Time: 85 ms (99.98%), Space: 16.6 MB (69.64%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,16 +1,23 @@
 class Solution:
-    def maxSumAfterPartitioning(self, arr, k):
-        N = len(arr)
-        K = k + 1
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        dp = [arr[0]] + [0]*((n:=len(arr)) -1)
+        currmax = arr[0]
 
-        dp = [0] * K
+        for i in range(1, k):
+            if arr[i] > currmax:
+                currmax = arr[i]
+            dp[i] = currmax * (i + 1)
 
-        for start in range(N - 1, -1, -1):
-            curr_max = 0
-            end = min(N, start + k)
+        for i in range(k, n):
+            curr = 0
+            currmax = -1
+            for j in range(1, k + 1):
+                if arr[i - j + 1] > currmax:
+                    currmax = arr[i - j + 1]
 
-            for i in range(start, end):
-                curr_max = max(curr_max, arr[i])
-                dp[start % K] = max(dp[start % K], dp[(i + 1) % K] + curr_max * (i - start + 1))
+                curr = dp[i-j] + (j*currmax)
 
-        return dp[0]
+                if curr > dp[i]:
+                    dp[i] = curr
+                    
+        return dp[n - 1]
