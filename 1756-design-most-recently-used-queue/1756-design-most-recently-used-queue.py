017901a338710931360c3466dbacf4eec236a038
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-12 14:51:59 -0400

    Time: 109 ms (95.33%), Space: 17.7 MB (91.59%) - LeetHub

--- 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.py
+++ 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.py
@@ -1,28 +1,8 @@
-class ListNode:
-    def __init__(self, val, next):
-        self.val = val
-        self.next = next
-
 class MRUQueue:
 
     def __init__(self, n: int):
-        self.head = ListNode(0, None)
-        curr = self.head
-        for i in range(1, n+1):
-            curr.next = ListNode(i, None)
-            curr = curr.next
-        self.tail = curr
+        self.data = list(range(1, n+1))
 
     def fetch(self, k: int) -> int:
-        curr = self.head
-        while k > 1:
-            curr = curr.next
-            k -= 1
-
-        mru = curr.next
-        if curr.next.next:
-            curr.next = curr.next.next
-            self.tail.next = mru
-            self.tail = self.tail.next
-            mru.next = None
-        return mru.val
+        self.data.append(self.data.pop(k-1))
+        return self.data[-1]
