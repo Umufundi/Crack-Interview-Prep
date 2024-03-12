fa61752cf946b4545444267f7a59354468bab71b
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-12 14:08:27 -0400

    Time: 68 ms (86.24%), Space: 18.2 MB (74.10%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,11 +1,26 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        res = []
-        n = len(graph)
-        def dfs(node,cur):
-            if node == n - 1:
-                res.append(cur)
-            for a in graph[node]:
-                dfs(a, cur+[a])
+        ans=[]
+        n=len(graph)
+        grp=defaultdict(list)
+        for i,g in enumerate(graph):
+            for node in g:
+                grp[i].append(node)
+       
+        
+        q=[]
+        heapq.heappush(q,(0))
+        
+        visit=set()
+
+        def dfs(node,res):
+            
+            if node==n-1:
+                    ans.append(res)
+                   
+            for nei in graph[node]:
+                    dfs(nei,res+[nei])
+            return 
+            
         dfs(0,[0])
-        return res
+        return ans 
