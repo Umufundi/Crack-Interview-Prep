b711d872b67f3ff9c5f3ffedc81ec99800ec1146
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-12 14:17:18 -0400

    Time: 34 ms (84.15%), Space: 16.6 MB (84.82%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,14 +1,8 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        if n==1: 
-            return 1
-        elif k==1:
-            return n
-        elif n>=k:
-            next_n = n - n//k
-            z = self.findTheWinner(next_n, k) - 1
-            x = (z-n%k + next_n) % next_n
-            return x + x//(k-1) + 1
-        else:
-            return (k + self.findTheWinner(n-1, k) -1) % n + 1
-        
+        friends = list(range(1, n+1))
+        index = 0
+        while len(friends) > 1:
+            index = (index + k - 1) % len(friends)
+            del friends[index]
+        return friends[0]
