3487d4b7bfa47c62be16dd6e7852ee492956172d
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-13 16:14:04 -0400

    Time: 48 ms (28.79%), Space: 16.6 MB (45.49%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,17 +1,14 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
         left_to_right = True
-        interval = 1 # step in the algorithm
-        last_remaining = n # last number in array
-        
-        # length of the array is > 1
+        step_counter = 1
+        last_remaining = n
         while n > 1:
-            # if array length is odd or going right to left
             if n%2 == 1 or not left_to_right:
-                last_remaining = last_remaining - interval # subtract interval
+                last_remaining = last_remaining - step_counter
             
-            n = int(n/2) # get new size of array
+            n = int(n/2)
             left_to_right = not left_to_right
-            interval *= 2 # get new interval
+            step_counter *= 2
         
         return last_remaining
