06fc5eb0b0e2a93ea69b3513d7a66056161e82b4
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-13 16:14:51 -0400

    Time: 48 ms (28.79%), Space: 16.6 MB (85.80%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.py
+++ 0390-elimination-game/0390-elimination-game.py
@@ -1,14 +1,6 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        left_to_right = True
-        step_counter = 1
-        last_remaining = n
-        while n > 1:
-            if n%2 == 1 or not left_to_right:
-                last_remaining = last_remaining - step_counter
-            
-            n = int(n/2)
-            left_to_right = not left_to_right
-            step_counter *= 2
-        
-        return last_remaining
+        arr = range(n,0,-1)
+        while len(arr)>1:
+            arr = arr[-2::-2]       
+        return arr[0]
