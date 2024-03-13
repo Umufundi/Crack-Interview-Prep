64e46473fd922e07288d8a2bb1b7ef8cb9e35bda
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-13 16:10:33 -0400

    Time: 914 ms (94.57%), Space: 22 MB (87.81%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,18 +1,13 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        total_gain = 0
-        curr_gain = 0
-        answer = 0
+        if sum(gas) < sum(cost): return -1
+        station = 0
+        Sum = 0
         
         for i in range(len(gas)):
-            # gain[i] = gas[i] - cost[i]
-            total_gain += gas[i] - cost[i]
-            curr_gain += gas[i] - cost[i]
-
-            # If we meet a "valley", start over from the next station
-            # with 0 initial gas.
-            if curr_gain < 0:
-                curr_gain = 0
-                answer = i + 1
+            Sum += gas[i] - cost[i]
+            if Sum < 0:
+                Sum = 0
+                station = i + 1
         
-        return answer if total_gain >= 0 else -1
+        return station
