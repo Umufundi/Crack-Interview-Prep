77ff58809ac3266fce81355f7f2e29d21ae28418
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-16 16:41:41 -0400

    Time: 87 ms (77.33%), Space: 18.3 MB (68.65%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findOrder(self, N, P):
+        G, indegree, ans = defaultdict(list), [0]*N, []
+        for nxt, pre in P:
+            G[pre].append(nxt)
+            indegree[nxt] += 1
+        
+        def dfs(cur):
+            ans.append(cur)
+            indegree[cur] = -1
+            for nextCourse in G[cur]:
+                indegree[nextCourse] -= 1
+                if indegree[nextCourse] == 0: 
+                    dfs(nextCourse)            
+        for i in range(N):
+            if indegree[i] == 0:
+                dfs(i)
+
+        return ans if len(ans) == N else []
