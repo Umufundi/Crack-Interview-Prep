09fb18a2511971d2d98800b8921fcd853ee2d486
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-16 19:55:20 -0400

    Time: 38 ms (92.56%), Space: 16.7 MB (29.93%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,23 +1,22 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # Creating Dictionary for Lookup
-        num_map = {
-            1: "I",
-            5: "V",    4: "IV",
-            10: "X",   9: "IX",
-            50: "L",   40: "XL",
-            100: "C",  90: "XC",
-            500: "D",  400: "CD",
-            1000: "M", 900: "CM",
-        }
-        
-        # Result Variable
-        r = ''
-        
-        
-        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
-            # If n in list then add the roman value to result variable
-            while n <= num:
-                r += num_map[n]
-                num-=n
-        return r
+        self.roman=''
+    
+        for i, magnitude in enumerate([1000, 100, 10, 1]):
+            factor= int(num /magnitude)
+            num = num %magnitude
+            self.roman+= self.get_letter(factor, i)
+
+        return self.roman
+
+    def get_letter(self, factor, i):
+        letters= ['M','C', 'X', 'I']
+        letters5= ['','D', 'L', 'V']
+        if factor<4:
+            return letters[i]*factor
+        elif factor==4:
+            return letters[i]+letters5[i]
+        elif factor>=5 and factor<9:
+            return letters5[i]+ (factor-5)*letters[i]
+        elif factor==9:
+            return letters[i]+letters[i-1]
