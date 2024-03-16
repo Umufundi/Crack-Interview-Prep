b1777dbe8e9db08a85c4843528dc1a33cdf0b45b
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-16 16:57:12 -0400

    Time: 32 ms (79.65%), Space: 16.6 MB (47.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,24 +1,15 @@
 class Solution:
-    
     def rob(self, nums: List[int]) -> int:
-        
-        # Special handling for empty case.
-        if not nums:
-            return 0
-        
-        N = len(nums)
-        
-        rob_next_plus_one = 0
-        rob_next = nums[N - 1]
-        
-        # DP table calculations.
-        for i in range(N - 2, -1, -1):
-            
-            # Same as recursive solution.
-            current = max(rob_next, rob_next_plus_one + nums[i])
-            
-            # Update the variables
-            rob_next_plus_one = rob_next
-            rob_next = current
-            
-        return rob_next
+        h1 , h2 = 0 , 0
+        #nums = [house1, house2, n, n+1]
+        for n in nums:
+            tmp = max(h1+n, h2)
+            h1 = h2
+            h2 = tmp
+        return h2
+        # sum1 = sum2 = 0
+        # for i in range(0, len(nums), 2):
+        #     sum1+= nums[i]
+        # for j in range(1, len(nums), 2):
+        #     sum2+= nums[j]
+        # return sum1 if sum1> sum2 else sum2
