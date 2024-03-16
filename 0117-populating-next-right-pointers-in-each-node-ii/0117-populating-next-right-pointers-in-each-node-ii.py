19fe8d4e3d81881a7de96eac13623e668923c0c7
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-16 17:17:08 -0400

    Time: 38 ms (87.43%), Space: 17.7 MB (28.73%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -1,67 +1,22 @@
-"""
-# Definition for a Node.
+"""# Definition for a Node.
 class Node:
     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
         self.val = val
         self.left = left
         self.right = right
-        self.next = next
-"""
-
+        self.next = next"""
 class Solution:
-    
-    def processChild(self, childNode, prev, leftmost):
-        if childNode:
-            
-            # If the "prev" pointer is alread set i.e. if we
-            # already found atleast one node on the next level,
-            # setup its next pointer
-            if prev:
-                prev.next = childNode
-            else:    
-                # Else it means this child node is the first node
-                # we have encountered on the next level, so, we
-                # set the leftmost pointer
-                leftmost = childNode
-            prev = childNode 
-        return prev, leftmost
-    
-    def connect(self, root: Optional['Node']) -> Optional['Node']:
-        
-        if not root:
-            return root
-        
-        # The root node is the only node on the first level
-        # and hence its the leftmost node for that level
-        leftmost = root
-        
-        # We have no idea about the structure of the tree,
-        # so, we keep going until we do find the last level.
-        # The nodes on the last level won't have any children
-        while leftmost:
-            
-            # "prev" tracks the latest node on the "next" level
-            # while "curr" tracks the latest node on the current
-            # level.
-            prev, curr = None, leftmost
-            
-            # We reset this so that we can re-assign it to the leftmost
-            # node of the next level. Also, if there isn't one, this
-            # would help break us out of the outermost loop.
-            leftmost = None
-            
-            # Iterate on the nodes in the current level using
-            # the next pointers already established.
-            while curr:
-                
-                # Process both the children and update the prev
-                # and leftmost pointers as necessary.
-                prev, leftmost = self.processChild(curr.left, prev, leftmost)
-                prev, leftmost = self.processChild(curr.right, prev, leftmost)
-                
-                # Move onto the next node.
-                curr = curr.next
-                
-        return root 
-            
-        
+    def connect(self, root: 'Node') -> 'Node':
+        q = []
+        q.append(root)
+        while q:
+            level = q.copy()
+            q.clear()
+            for i in range(0, len(level)):
+                if level[i] is None:continue    
+                if i == len(level)-1:level[i].next = None
+                else:level[i].next = level[i+1]  
+                if level[i].left:q.append(level[i].left)
+                if level[i].right:
+                    q.append(level[i].right)
+        return root
