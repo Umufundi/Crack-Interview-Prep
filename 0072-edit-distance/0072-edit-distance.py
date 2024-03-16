352c39154680b794d7fb2fcf41fd28e6694938d1
Flo <96528163+Umufundi@users.noreply.github.com>
2024-03-16 18:01:58 -0400

    Time: 36 ms (100.00%), Space: 16.6 MB (96.00%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,21 +1,22 @@
 class Solution:
-  def minDistance(self, word1: str, word2: str) -> int:
-    m = len(word1)
-    n = len(word2)
-    # dp[i][j] := min # Of operations to convert word1[0..i) to word2[0..j)
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-
-    for i in range(1, m + 1):
-      dp[i][0] = i
-
-    for j in range(1, n + 1):
-      dp[0][j] = j
-
-    for i in range(1, m + 1):
-      for j in range(1, n + 1):
-        if word1[i - 1] == word2[j - 1]:
-          dp[i][j] = dp[i - 1][j - 1]
-        else:
-          dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
-
-    return dp[m][n]
+    def minDistance(self, word1: str, word2: str) -> int:
+        na,nb = len(word1),len(word2)
+        if not word1 or not word2:
+            return max(na,nb)
+        q = deque([(0,0)])
+        steps = 0
+        seen = set()
+        while q:
+            for _ in range(len(q)):
+                i,j = q.popleft()
+                while i<na and j<nb and word1[i] == word2[j]:
+                    i += 1
+                    j += 1 
+                if i == na and j == nb: return steps
+                for da,db in [(0,1),(1,0),(1,1)]:
+                    ni,nj = i+da,j+db
+                    if ni<=na and nj<=nb and (ni,nj) not in seen:
+                        seen.add((ni,nj))
+                        q.append((ni,nj))
+            steps+=1
+        return steps
